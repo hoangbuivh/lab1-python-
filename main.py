8b7e3e0f07577d831ca7a0c55834b5111d6e1778
Hoang  Bui <hhb5051@psu.edu>
2020-09-01 21:20:42 +0000

    add fah and cel

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ temp = float(input("Enter temperature: "))
 unit = (input("Enter unit in F/f or C/c: "))
 if unit == 'C' or unit == 'c': 
   fah = (temp * 9/5) + 32
-  print(f'{temp}° in Fahrenheit is equilvalent to {fah}°')
+  print(f'{temp}° in Fahrenheit is equilvalent to {fah}° Fahrenheit')
 
 elif unit == 'F' or unit == 'f': 
   cel = (temp-32) / (9/5)
-  print(f'{temp}° in Celsius is equilvalent to {cel}°')
+  print(f'{temp}° in Celsius is equilvalent to {cel}° Celsius')
 
 else: 
   print("invalid unit(bad)")

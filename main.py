dea519c811145a547ba128179a1d778c5e000aee
Hoang  Bui <hhb5051@psu.edu>
2020-09-01 21:28:41 +0000

    add author + collaborator

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Author Hoang Bui hhb5051@psu.edu  
+# Collaborator Adalia Chen asc209@psu.edu
+# Collaborator Ved Walavalkar vtw5023@psu.edu   
+
 temp = float(input("Enter temperature: "))
 unit = (input("Enter unit in F/f or C/c: "))
 if unit == 'C' or unit == 'c': 
